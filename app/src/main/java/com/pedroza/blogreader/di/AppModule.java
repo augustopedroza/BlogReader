d063c4b8b27232a39acccdaaca5c397f38f313d6
Augusto Pedroza <augusto.pedroza@gmail.com>
2017-12-07 11:47:33 -0700

    Added basic network support - work in progress

--- app/src/main/java/com/pedroza/blogreader/di/AppModule.java
+++ app/src/main/java/com/pedroza/blogreader/di/AppModule.java
@@ -1,6 +1,17 @@
 package com.pedroza.blogreader.di;
 
+import com.pedroza.blogreader.api.InVisionService;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by pedroza on 12/7/17.
@@ -8,5 +19,25 @@ import dagger.Module;
 
 @Module
 public class AppModule {
+    @Singleton
+    @Provides
+    InVisionService provideInVisionService() {
+
+        //
+        //  OkHttp3 injector allows me to add the api_key paramter to every request
+        //
+
+        return new Retrofit.Builder()
+                .baseUrl("http://engineering.invisionapp.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(new OkHttpClient.Builder()
+                        .readTimeout(20_000, TimeUnit.MILLISECONDS)
+                        .writeTimeout(20_000, TimeUnit.MILLISECONDS)
+                        .connectTimeout(20_000, TimeUnit.MILLISECONDS)
+                        .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.HEADERS))
+                        .build())
+                .build()
+                .create(InVisionService.class);
+    }
 
 }
--- app/src/main/java/com/pedroza/blogreader/repository/BlogEntriesRepository.java
+++ app/src/main/java/com/pedroza/blogreader/repository/BlogEntriesRepository.java
@@ -1,9 +1,57 @@
 package com.pedroza.blogreader.repository;
 
+import android.arch.lifecycle.MutableLiveData;
+
+import com.pedroza.blogreader.api.InVisionService;
+import com.pedroza.blogreader.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 /**
  * Created by pedroza on 12/7/17.
  */
 
 public class BlogEntriesRepository {
+
+    private InVisionService mInvisionService;
+
+    private MutableLiveData<List<Item>> mItems;
+
+    private List<Item> mItemsInternal;
+
+    @Inject
+    public BlogEntriesRepository(InVisionService inVisionService) {
+
+        mInvisionService = inVisionService;
+        mItems = new MutableLiveData<>();
+        mItemsInternal = new ArrayList<>();
+    }
+
+    public MutableLiveData<List<Item>> getItems() {
+        return mItems;
+    }
+
+    public void loadBlogEntries() {
+        Call<ResponseBody> loadBlogEntriesCall = mInvisionService.getInvisionRss();
+        loadBlogEntriesCall.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                response.body();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+    }
 }
 
